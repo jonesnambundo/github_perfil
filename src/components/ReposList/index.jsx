4f96b2dc8e59156ed3950ed57d42d320586180e5
jonesnambunndo@hotmail.com <jonesnambundo@mtel.inatel.br>
2023-12-26 22:42:02 -0300

    atualizado

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -1,31 +1,43 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from './ReposList.module.css';
 
 const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
+    const [erro, setErro] = useState(null);
 
     useEffect(() => {
         setEstaCarregando(true);
-        fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-        .then(res => res.json())
-        .then(resJson => {
-            setTimeout(() => {
-                setEstaCarregando(false);
-                setRepos(resJson);
-            }, 3000);
+        setErro(null);
 
-        })
+        fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
+            .then(res => {
+                if (res.status === 404) {
+                    throw new Error("Usuário não encontrado");
+                }
+                return res.json();
+            })
+            .then(resJson => {
+                setTimeout(() => {
+                    setEstaCarregando(false);
+                    setRepos(resJson);
+                }, 3000);
+            })
+            .catch(error => {
+                setEstaCarregando(false);
+                setErro(error.message);
+            });
     }, [nomeUsuario]);
 
     return (
         <div className="container">
-            {estaCarregando ? (
+            {erro ? (
+                <h1>{erro}</h1>
+            ) : estaCarregando ? (
                 <h1>Carregando...</h1>
             ) : (
                 <ul className={styles.list}>
-                    {/* {repos.map(repositorio => ( */}
                     {repos.map(({ id, name, language, html_url }) => (
                         <li className={styles.listItem} key={id}>
                             <div className={styles.itemName}>
@@ -36,13 +48,15 @@ const ReposList = ({ nomeUsuario }) => {
                                 <b>Linguagem:</b>
                                 {language}
                             </div>
-                            <a className={styles.itemLink} target="_blank" href={html_url}>Visitar no Github</a>
+                            <a className={styles.itemLink} target="_blank" href={html_url}>
+                                Visitar no Github
+                            </a>
                         </li>
                     ))}
                 </ul>
             )}
         </div>
-    )
-}
+    );
+};
 
 export default ReposList;
